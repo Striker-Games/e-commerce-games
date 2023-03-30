768657e62194a040a2e840e5d8cbaafc8aca6308
pabloTokuo <contatopbo@hotmail.com>
2023-03-30 15:38:13 -0300

    banners atualizados

--- js/script.js
+++ js/script.js
@@ -20,5 +20,32 @@ $(document).ready(function(){
               items:5
           }
       }
-  })
+  }),
+  $('.owl-carousel2').owlCarousel({
+    loop:true,
+    margin:5,
+    dots: false,
+    items: 1
+})
 });
+
+const slider = document.querySelector(".swiper").swiper;
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3500
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
