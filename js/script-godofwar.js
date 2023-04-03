94e7ab7fd721d2f3c53deaf085995718130030fe
pabloTokuo <contatopbo@hotmail.com>
2023-04-03 16:09:44 -0300

    Site responsivo

--- js/script-godofwar.js
+++ js/script-godofwar.js
@@ -1,10 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
   });
 
 const responsivo = document.querySelector(".responsivo");
