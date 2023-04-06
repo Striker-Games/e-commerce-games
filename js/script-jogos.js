c5dfd4197357c8a53900b5a1bbbb8bcb1312e92d
pabloTokuo <contatopbo@hotmail.com>
2023-04-06 10:40:40 -0300

    Carrossel de imagens responsivo

--- js/script-jogos.js
+++ js/script-jogos.js
@@ -1,9 +1,19 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: 'true',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
     },
   });
 
