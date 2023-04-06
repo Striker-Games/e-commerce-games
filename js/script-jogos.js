95b0e32dbe89e1723051f93096ebe4dcc0928d38
pabloTokuo <contatopbo@hotmail.com>
2023-04-05 22:06:28 -0300

    Imagem expande ao click

--- js/script-jogos.js
+++ js/script-jogos.js
@@ -11,3 +11,14 @@ const responsivo = document.querySelector(".responsivo");
 if (window.innerWidth <= 755) {
     responsivo.classList.toggle("responsivo");
 }
+
+document.querySelectorAll(".swiper-background img").forEach(image => {
+  image.onclick = () => {
+    document.querySelector(".popup-image").style.display = 'block';
+    document.querySelector('.popup-image img').src = image.getAttribute('src');
+  }
+});
+
+document.querySelector('.popup-image span').onclick = () => {
+  document.querySelector(".popup-image").style.display = 'none';
+}
